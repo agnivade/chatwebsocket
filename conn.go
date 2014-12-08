a6ddef5a611b5958b4b09fceb13924d707ff2340
Agniva De Sarker <adesarkar@instartlogic.com>
2014-12-09 00:26:25 +0530

    Adding support for html page detection and sending of image
    - Detecting the page with just a string match
    - Parsing the html tree and finding out the image node
    - Sending it to the browser
    
    Reviewed by:
    Testing:
    Tickets:
    Backport:

--- conn.go
+++ conn.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	// "io/ioutil"
 	"log"
+	"net/http"
 	"strings"
 
 	"github.com/gorilla/websocket"
+	"golang.org/x/net/html"
 )
 
 type connection struct {
@@ -32,22 +35,65 @@ type message struct {
 
 func (c *connection) reader() {
 	for {
-		_, msg, err := c.ws.ReadMessage()
+		_, ws_msg, err := c.ws.ReadMessage()
 		if err != nil {
 			log.Println("Error occured- ", err)
 			break
 		}
-		log.Printf("Message received - %s\n", msg)
-		s := string(msg[:])
-		msg_array := strings.Split(s, ":")
+		log.Printf("Message received - %s\n", ws_msg)
+		s := string(ws_msg[:])
+		msg_array := strings.Split(s, "^")
 		if msg_array[0] == "0" {
 			// send userreq to userchan
 			user := &userreq{conn: c, username: msg_array[1], password: msg_array[2]}
 			h.userchan <- user
-		} else if msg_array[0] == "1" {
-			// send to broadcast chan
-			msg := &message{target_user: msg_array[1], message: msg_array[2], conn: c}
-			h.broadcast <- msg
+		} else if msg_array[0] == "1" { // send to broadcast chan
+			// if the string contains an url, extract it
+			// This is a simple check for the sake of time
+			if strings.Contains(msg_array[2], "http:") {
+
+				// get the html content from the page
+				resp, err := http.Get(msg_array[2])
+				if err != nil {
+					log.Println("Error occured while fetching from url- ", err)
+					break
+				}
+				defer resp.Body.Close()
+				// body, err := ioutil.ReadAll(resp.Body)
+				// log.Println(string(body))
+				// continue
+				msgToSend := ""
+				// get the first image tag
+				doc, err := html.Parse(resp.Body)
+				if err != nil {
+					log.Println("Error occured while parsing- ", err)
+					break
+				}
+				var f func(*html.Node)
+				f = func(n *html.Node) {
+					if n.Type == html.ElementNode && n.Data == "img" {
+						//Found the image node !
+						for _, tag_attr := range n.Attr {
+							if tag_attr.Key == "src" {
+								//Found the src attribute !
+								msgToSend = "^" + tag_attr.Val
+								return
+							}
+						}
+					}
+					for c := n.FirstChild; c != nil; c = c.NextSibling {
+						f(c)
+					}
+				}
+				f(doc)
+				// construct the message
+				msg := &message{target_user: msg_array[1], message: msgToSend, conn: c}
+
+				h.broadcast <- msg
+			} else {
+				msg := &message{target_user: msg_array[1], message: msg_array[2], conn: c}
+				h.broadcast <- msg
+			}
 		}
 
 	}
